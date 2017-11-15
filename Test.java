e09cef90a3c19f98d956127ff7b786eb023a2f6f
Manjunath Baburao <manjunathbaburao@users.noreply.github.com>
2017-11-15 17:18:19 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,24 +1,23 @@
-	private AmazonS3 getS3Client(){
-		AWSCredentials credentials = new BasicAWSCredentials(accessKeyId, secretAccessKey);
-		AmazonS3 s3client = null;
-		if(proxyEnabled){
-			logger.info("Using proxy");
-			ClientConfiguration config = new ClientConfiguration();
-			config.setProxyDomain("proxy.wdf.sap.corp");
-			config.setProxyPort(8080);
-			s3client = new AmazonS3Client(credentials, config);
-		}else{
-			logger.info("Not using proxy");
-			s3client = new AmazonS3Client(credentials);
-		}
+private AmazonS3 getS3Client(){
+	AWSCredentials credentials = new BasicAWSCredentials(accessKeyId, secretAccessKey);
+	AmazonS3 s3client = null;
+	if(proxyEnabled){
+		logger.info("Using proxy");
+		ClientConfiguration config = new ClientConfiguration();
+		config.setProxyDomain("proxy.wdf.sap.corp");
+		config.setProxyPort(8080);
+		s3client = new AmazonS3Client(credentials, config);
+	}else{
+		logger.info("Not using proxy");
+		s3client = new AmazonS3Client(credentials);
+	}
 		s3client.setEndpoint(awsS3Endpoint);
 		return  s3client;
-	}
+  }
   
   public String uploadObject(@PathVariable("bucket_name") String bucketName,
-            HttpServletRequest request, HttpServletResponse response){
-    	
-    	String objectName = "object"+count; 
+  HttpServletRequest request, HttpServletResponse response){
+      	String objectName = "object"+count; 
     	
     	AmazonS3 s3client = getS3Client();
     	ClassLoader classLoader = this.getClass().getClassLoader();
@@ -28,11 +27,10 @@
     	count++;
     	return "successfully created object "+objectName;
 		
-	}
+  }
   
-  public void downloadObject(@PathVariable("bucket_name") String bucketName,
-			@PathVariable("object_name") String objectName,
-            HttpServletRequest request, HttpServletResponse response) throws IOException{
+  public void downloadObject(@PathVariable("bucket_name") String bucketName,@PathVariable("object_name") String objectName,
+  HttpServletRequest request, HttpServletResponse response) throws IOException{
     	
     	AmazonS3 s3client = getS3Client();
     	S3Object seObject = s3client.getObject(new GetObjectRequest(bucketName, objectName));
@@ -43,4 +41,4 @@
     	response.addHeader("metadata", mapper.writeValueAsString(objectMetadata));
     	org.apache.commons.io.IOUtils.copy(seObject.getObjectContent(), response.getOutputStream());
     	
-	}
+  }
